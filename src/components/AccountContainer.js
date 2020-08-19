eea4734e9ef2f259a95e3170a7294d33835d14fd
Tien Phan <tienphan1989@gmail.com>
2020-08-19 11:22:03 -0400

    Done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,60 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const url = `http://localhost:3000/transactions`;
+
 class AccountContainer extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       transactions: [],
+       displayTransactions: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(url)
+    .then(resp => resp.json())
+    .then(bankData => this.setState({
+      transactions: bankData,
+      displayTransactions: bankData
+    }))
+  }
+
+  handleForm = (transaction) => {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(transaction)
+    })
+    .then(resp => resp.json())
+    .then(newTransaction => {
+      this.setState({
+        transactions: [...this.state.transactions, newTransaction],
+        displayTransactions: [...this.state.displayTransactions, newTransaction]
+      })
+    })
+  }
+
+  handleSearch = (query) => {
+    let filteredList = this.state.transactions.filter(transaction => transaction.description.toLowerCase().includes(query.toLowerCase()))
+
+    this.setState({
+      displayTransactions: filteredList
+    })
+  }
+
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search handleSearch={this.handleSearch}/>
+        <AddTransactionForm handleForm={this.handleForm}/>
+        <TransactionsList displayTransactions={this.state.displayTransactions}/>
       </div>
     );
   }
